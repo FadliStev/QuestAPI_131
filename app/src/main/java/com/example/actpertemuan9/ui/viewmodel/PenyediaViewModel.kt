c56344158041ce698b52909fbe8aaef16208c558
Wiranto Akbar Fadlillah <alvinex1999@gmail.com>
2024-12-29 09:05:09 +0700

    menambah penyedia view model

---
+++ app/src/main/java/com/example/actpertemuan9/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.actpertemuan9.ui.viewmodel
+
+import android.text.Spannable.Factory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.actpertemuan9.MahasiswaApp
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+        initializer { InsertViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+    }
+}
+
+fun CreationExtras.aplikasiMahasiswa(): MahasiswaApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApp)

8fd2cc6aa7fd5e7c0a3c0cfaf7a5df5ddcacda61
Wiranto Akbar Fadlillah <alvinex1999@gmail.com>
2024-12-30 11:07:35 +0700

    menambah initializer

--- app/src/main/java/com/example/actpertemuan9/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/actpertemuan9/ui/viewmodel/PenyediaViewModel.kt
@@ -2,6 +2,7 @@ package com.example.actpertemuan9.ui.viewmodel
 
 import android.text.Spannable.Factory
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -11,6 +12,8 @@ object PenyediaViewModel{
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
         initializer { InsertViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+        initializer { DetailViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+        initializer { UpdateViewModel(createSavedStateHandle(),aplikasiMahasiswa().container.mahasiswaRepository) }
     }
 }
 

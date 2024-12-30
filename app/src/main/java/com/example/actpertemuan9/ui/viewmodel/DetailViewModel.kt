95fb7e478d8935735520faf76541942c009a2b9a
Wiranto Akbar Fadlillah <alvinex1999@gmail.com>
2024-12-30 10:50:09 +0700

    menambah data class

--- app/src/main/java/com/example/actpertemuan9/ui/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/actpertemuan9/ui/viewmodel/DetailViewModel.kt
@@ -2,6 +2,21 @@ package com.example.actpertemuan9.ui.viewmodel
 
 import com.example.actpertemuan9.data.Mahasiswa
 
+
+
+
+
+
+data class DetailUiState(
+    val detailUiEvent: InsertUiEvent = InsertUiEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = "",
+){
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != InsertUiEvent()
+}
+
 fun Mahasiswa.toDetailEvent(): InsertUiEvent{
     return InsertUiEvent(
         nim = nim,

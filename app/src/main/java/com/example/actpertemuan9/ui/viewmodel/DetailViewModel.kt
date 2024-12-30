3c73c800003f66731cc2b922fb6fc72f6b380954
Wiranto Akbar Fadlillah <alvinex1999@gmail.com>
2024-12-30 10:53:59 +0700

    menambah detail view model

--- app/src/main/java/com/example/actpertemuan9/ui/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/actpertemuan9/ui/viewmodel/DetailViewModel.kt
@@ -1,11 +1,34 @@
 package com.example.actpertemuan9.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.actpertemuan9.data.Mahasiswa
+import com.example.actpertemuan9.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
 
 
+class DetailViewModel (
+    private val mhsRepository: MahasiswaRepository
+): ViewModel(){
+    var uiState by mutableStateOf(DetailUiState())
+        private set
 
-
-
+    fun fetchDetailMahasiswa(nim: String) {
+        viewModelScope.launch {
+            uiState = DetailUiState(isLoading = true)
+            try {
+                val mahasiswa = mhsRepository.getMahasiswaById(nim)
+                uiState = DetailUiState(detailUiEvent = mahasiswa.toDetailEvent())
+            } catch (e: Exception) {
+                e.printStackTrace()
+                uiState = DetailUiState(isError = true, errorMessage = "Failed to fetch details: ${e.message}")
+            }
+        }
+    }
+}
 
 data class DetailUiState(
     val detailUiEvent: InsertUiEvent = InsertUiEvent(),
